c4c1c56a2ad8d1731093ede92483e97fadac771b
Jon Packer <contact@jonpacker.com>
2013-04-14 15:14:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# SRM2HEX
+
+Converts an SRM value to hex.
+
+### install
+
+```
+npm install srm2hex 
+```
+
+### use
+```javascript
+var srm2hex = require('srm2hex');
+srm2hex(20.5); // -> #562e11
+```
+
+### license
+
+MIT
---
+++ index.js
@@ -0,0 +1,7 @@
+var lookup = require('./lookup');
+module.exports = function(srm) {
+  if (srm < 0.1) return '#FFFFFF';
+  if (srm > 40) return '#000000';
+  var rgb = lookup[Math.floor(srm * 10)];
+  return '#' + rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16);
+};
